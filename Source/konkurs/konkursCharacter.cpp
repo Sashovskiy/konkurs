8da82e0f23e4765955fc851b68896cd8fae92b14
DESKTOP-89K51GE\Sas <oleksandr126@gmail.com>
2018-08-15 01:41:03 +0300

    2

--- Source/konkurs/konkursCharacter.cpp
+++ Source/konkurs/konkursCharacter.cpp
@@ -49,12 +49,12 @@ AkonkursCharacter::AkonkursCharacter()
 	GetCharacterMovement()->bOrientRotationToMovement = false;
 
 	// Configure character movement
-	GetCharacterMovement()->GravityScale = 2.0f;
+	GetCharacterMovement()->GravityScale = 0.5f;
 	GetCharacterMovement()->AirControl = 0.80f;
-	GetCharacterMovement()->JumpZVelocity = 1000.f;
+	GetCharacterMovement()->JumpZVelocity = 3000.f;
 	GetCharacterMovement()->GroundFriction = 3.0f;
 	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
-	GetCharacterMovement()->MaxFlySpeed = 600.0f;
+	GetCharacterMovement()->MaxFlySpeed = 2600.0f;
 
 	// Lock character motion onto the XZ plane, so the character can't move in or out of the screen
 	GetCharacterMovement()->bConstrainToPlane = true;
